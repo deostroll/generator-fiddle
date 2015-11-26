92bb003d5c27e48907a514444b731feec8cebe4f
deostroll <deostroll@gmail.com>
2015-11-26 17:02:33 +0530

    plain fiddle complete

--- app/index.js
+++ app/index.js
@@ -2,11 +2,11 @@
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-
+var mkdirp = require('mkdirp');
 var FiddleGenerator = yeoman.generators.Base.extend({
     init: function () {
         this.pkg = require('../package.json');
-
+        this.mkdir = mkdirp;
         this.devDependencies = [
             //commn dependencies
             'grunt',
@@ -17,6 +17,8 @@ var FiddleGenerator = yeoman.generators.Base.extend({
 
         this.on('end', function(){
           console.log(chalk.green('All done...'));
+          console.log();
+          console.log('type "grunt" to start a local web server');
         });
     },
 
@@ -53,23 +55,25 @@ var FiddleGenerator = yeoman.generators.Base.extend({
         this.mkdir('app/images');
         this.mkdir('app/styles');
         this.mkdir('app/scripts');
-        // this.mkdir('app/less');
-        // console.log(this.props);
-        // this.copy('app/index.html', 'app/index.html', this.props);
 
         this.fs.copyTpl(
           this.templatePath('app/index.html'),
           this.destinationPath('app/index.html'),
           this.props
         );
+
         this.copy('app/styles/style.css', 'app/styles/style.css');
         this.copy('app/scripts/main.js', 'app/scripts/main.js');
-
+        this.copy('app/images/yeoman.png', 'app/images/yeoman.png');
+        
         // We'll compose our grunt file
         // this.copy('Gruntfile.js', 'Gruntfile.js');
 
-        this.copy('package.json', 'package.json');
-        this.copy('bower.json', 'bower.json');
+        // this.copy('package.json', 'package.json');
+        // this.copy('bower.json', 'bower.json');
+        this.fs.copyTpl(this.templatePath('package.json'), 'package.json', this.props);
+        this.fs.copyTpl(this.templatePath('bower.json'), 'bower.json', this.props);
+
         this.copy('bowerrc', '.bowerrc');
         this.copy('README.md', 'README.md');
     },
@@ -125,11 +129,6 @@ var FiddleGenerator = yeoman.generators.Base.extend({
       ]);
 
       this.gruntfile.registerTask('default', ['connect', 'watch']);
-      // var sourceRoot = this.sourceRoot();
-      // var runbower = path.join(sourceRoot, 'misc', 'runbower.js')
-      //
-      // this.gruntfile.prependJavaScript('grunt.registerTask(\'runbower\', runbower)');
-      // this.gruntfile.prependJavaScript(this.fs.read(runbower));
     },
     installStuff: function() {
       this.npmInstall(this.devDependencies, {saveDev: true});
